00521108ea9aad35595138f8cb72e51d8839dc96
lijiabin01 <lijiabin01@baidu.com>
2018-05-23 17:05:26 +0800

    Home Icons

--- QunarApp/Travel/src/pages/home/components/Swiper.vue
+++ QunarApp/Travel/src/pages/home/components/Swiper.vue
@@ -16,7 +16,8 @@ export default{
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
-        loop: true
+        loop: true,
+        autoplay: 2000
       },
       swiperList: [{
         id: '0001',
--- QunarApp/Travel/src/pages/home/home.vue
+++ QunarApp/Travel/src/pages/home/home.vue
@@ -5,6 +5,7 @@
         <!-- Hello World -->
         <home-header></home-header>
         <home-swiper></home-swiper>
+        <home-icons></home-icons>
     </div>
 </template>
 
@@ -12,12 +13,14 @@
 // 局部组件
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 
 export default{
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
