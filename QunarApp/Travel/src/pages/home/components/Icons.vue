06380806e5f2a55628e45b6e88eeab50ad533247
lijiabin01 <lijiabin01@baidu.com>
2018-05-24 17:51:10 +0800

    add swiper icons

--- QunarApp/Travel/src/pages/home/components/Icons.vue
+++ QunarApp/Travel/src/pages/home/components/Icons.vue
@@ -1,99 +1,113 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+            <!-- <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt=""> -->
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default{
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票阿萨德1111222233333333'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '滑雪季埃里克森的就奥克兰删掉就路况'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '泡温泉'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '动植物园'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '滑雪季'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '泡温泉'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '动植物园'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '动植物园'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
-  .icons
-    width : 100%
-    overflow : hidden
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-container
+    // width : 100%
+    // overflow : hidden
     height : 0
     padding-bottom : 50%
-    .icon
-      position: relative
-      overflow: hidden
-      float: left
-      width: 25%
-      height : 0
-      padding-bottom : 25%
-      .icon-img
-        position : absolute
-        top :0
-        left : 0
-        bottom :.44rem
-        right : 0
-        .icon-img-content
-          display : block
-          margin : 0 auto
-          height : 100%
-      .icon-desc
-        position :absolute
-        left :0
-        right : 0
-        bottom : 0
-        height : .44rem
-        line-height : .44rem
-        // background : $darkTextColor
-        text-align : center
+  .icon
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%
+    height : 0
+    padding-bottom : 25%
+    .icon-img
+      position : absolute
+      top :0
+      left : 0
+      bottom :.44rem
+      right : 0
+      .icon-img-content
+        display : block
+        margin : 0 auto
+        height : 100%
+        background : green
+    .icon-desc
+      position :absolute
+      left :0
+      right : 0
+      bottom : 0
+      height : .44rem
+      line-height : .44rem
+      // background : $darkTextColor
+      text-align : center
+      ellipsis()
 </style>
