cdc83faf489e5706e012987bfef601bf6f1339d3
lijiabin01 <lijiabin01@baidu.com>
2018-05-30 16:47:16 +0800

    首页和城市列表页优化：不重复发送请求

--- QunarApp/Travel/src/pages/home/home.vue
+++ QunarApp/Travel/src/pages/home/home.vue
@@ -1,26 +1,22 @@
 <template>
-    <div>
-        <!-- <div class="home">home</div>
-        <router-link to="/list">列表页</router-link> -->
-        <!-- Hello World -->
-        <home-header :city='city'></home-header>
-        <home-swiper :list='swiperList'></home-swiper>
-        <home-icons :list='iconList'></home-icons>
-        <home-recommend :list='recommendList'></home-recommend>
-        <home-weekend :list='weekendList'></home-weekend>
-    </div>
+  <div>
+    <home-header></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
+  </div>
 </template>
 
 <script>
-// 局部组件
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
-
-export default{
+import { mapState } from 'vuex'
+export default {
   name: 'Home',
   components: {
     HomeHeader,
@@ -31,39 +27,45 @@ export default{
   },
   data () {
     return {
-      city: '',
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
       res = res.data
       if (res.ret && res.data) {
         const data = res.data
-        this.city = data.city
         this.swiperList = data.swiperList
         this.iconList = data.iconList
         this.recommendList = data.recommendList
         this.weekendList = data.weekendList
       }
-    //   console.log(res)
     }
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
 
 <style>
-    /* .home{
-        font-size: 50px;
-    } */
+
 </style>
